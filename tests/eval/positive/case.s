1f9c4434ba0b7ba5cda478d84754d0ead5f56159
Vilem Liepelt <v.liepelt@kent.ac.uk>
2019-06-11 18:38:24 +0100

    New test cases

---
+++ tests/eval/positive/case.s
@@ -0,0 +1,20 @@
+type T = A | B;
+type S = C(int f1, bool f2) | D(T f1);
+
+int i;
+bool b;
+T t;
+
+@initial
+f(S s) {
+  case s {
+    D(t1) -> t = t1;
+    C(0, b1) -> b = b1;
+    C(i1, false) -> i = i1;
+    C(_, _) -> t = B;
+  };
+  stay();
+}
+
+@initial
+g() { terminate() }
