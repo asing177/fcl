f32c038552bf6b9fe577da47fdd6a01cfa51315d
acentelles <centelles.alberto@gmail.com>
2019-07-03 10:41:30 +0100

    Remove unused imports

--- tests/TestSwagger.hs
+++ tests/TestSwagger.hs
@@ -1,20 +1,12 @@
-{-# OPTIONS_GHC -fno-warn-orphans #-}
-{-# LANGUAGE TypeApplications #-}
-
 module TestSwagger where
 
 import Protolude
 
 import Servant.Swagger.Test
 import Test.Hspec
-import Test.QuickCheck
 import Test.Hspec.QuickCheck
 import HTTP.FCL.API
-import Language.FCL.LanguageServerProtocol
-import Test.Tasty
-import Test.Tasty.QuickCheck
 
-import Data.Swagger.Schema.Validation
 swaggerTest :: Spec
 swaggerTest = modifyMaxSuccess (\_ -> 50)
   $ context "ToJSON matches ToSchema" $ validateEveryToJSON fclProxyAPI
