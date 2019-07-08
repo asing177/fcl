8b213bbee9557472c151110c5bdf68c6b7ea3384
anabra <peter.d.podlovics@gmail.com>
2019-07-08 14:52:17 +0100

    Added docstrings to workflow generation

--- src/Language/FCL/WorkflowGen.hs
+++ src/Language/FCL/WorkflowGen.hs
@@ -24,58 +24,74 @@ import Language.FCL.AST
 -- NOTE: can't return back to different places from a loop (eg.: novation.s)
 -- NOTE: syncronisation points are always singleton states (places) (eg.: novation.s)
 -- NOTE: there is a loop hidden inside GenXOR
+-- NOTE: GenXOR lhsIn lhsOut rhsIn rhsOut Nothing Nothing is equivalent to
+--       XOR (Seq lhsIn lhsOut) (Seq rhsIn rhsOut)
+
+-- | Workflow nets that are sound by construction. We only allow these _safe_ workflow nets
+-- to be constructed in very specific ways in order to make soundness verification automatic.
 data SafeWorkflowNet
-  = AND { andBranches :: NonEmpty SafeWorkflowNet }
-  | XOR
-  { xorLhs :: SafeWorkflowNet
-  , xorRhs :: SafeWorkflowNet
+  -- | AND splitting into multiple workflows.
+  = AND { andBranches :: NonEmpty SafeWorkflowNet    -- ^ At least one branch
+        }
+  -- | XOR splitting into two workflows.
+  | XOR { xorLhs :: SafeWorkflowNet                  -- ^ Left XOR branch
+        , xorRhs :: SafeWorkflowNet                  -- ^ Right XOR branch
+        }
+  -- |  Sequencing two workflow.
+  | Seq { seqLhs :: SafeWorkflowNet                  -- ^ First workflow
+        , seqRhs :: SafeWorkflowNet                  -- ^ Second workflow
+        }
+  -- | Looping construct with option to exit from both the body of the loop and the head of the loop.
+  -- If the first half of the body is empty, we exit from head, otherwise exit from the body.
+  | GenLoop { gLoopIn   :: Maybe SafeWorkflowNet     -- ^ First half of the body of the loop
+            , gLoopExit :: SafeWorkflowNet           -- ^ Exit from the loop
+            , gLoopOut  :: SafeWorkflowNet           -- ^ Seconds half of the body of the loop
   }
-  | Seq
-  { seqLhs :: SafeWorkflowNet
-  , seqRhs :: SafeWorkflowNet
-  }
-  -- looping construct with option to exit from the body of the loop
-  -- (can also exit from the start -- no gLoopIn)
-  | GenLoop
-  { gLoopIn   :: Maybe SafeWorkflowNet
-  , gLoopExit :: SafeWorkflowNet
-  , gLoopOut  :: SafeWorkflowNet
-  }
-  -- GenXOR lhsIn lhsOut rhsIn rhsOut Nothing Nothing is equivalent to
-  -- XOR (Seq lhsIn lhsOut) (Seq rhsIn rhsOut)
-  | GenXOR
-  { gXorLhsIn   :: SafeWorkflowNet
-  , gXorLhsOut  :: SafeWorkflowNet
-  , gXorRhsIn   :: SafeWorkflowNet
-  , gXorRhsOut  :: SafeWorkflowNet
-  -- communication between the different bracnhes
-  , gXorMToRhs  :: (Maybe SafeWorkflowNet)
-  , gXorMToLhs  :: (Maybe SafeWorkflowNet)
-  }
+  -- | General XOR split with the possibility of moving between the branches.
+  | GenXOR { gXorLhsIn   :: SafeWorkflowNet          -- ^ First half of left-hand side
+           , gXorLhsOut  :: SafeWorkflowNet          -- ^ Second half of left-hand side
+           , gXorRhsIn   :: SafeWorkflowNet          -- ^ First half of right-hand side
+           , gXorRhsOut  :: SafeWorkflowNet          -- ^ Second half of right-hand side
+           , gXorMToRhs  :: (Maybe SafeWorkflowNet)  -- ^ Moving to the right-hand side from the left one
+           , gXorMToLhs  :: (Maybe SafeWorkflowNet)  -- ^ Moving to the left-hand side from the right one
+           }
+  -- | Atom representing a single transition.
   | Atom
   deriving (Eq, Ord, Show, Generic)
 
 {-# COMPLETE XOR, AND, Seq, SimpleLoop, Loop, GenXOR #-}
+-- | XOR with three branches.
 pattern XOR3 a b c   = XOR a (XOR b c)
+-- | AND with two branches.
 pattern AND2 lhs rhs = AND (lhs :| [rhs])
+-- | Simple loop with exit only from the head.
 pattern SimpleLoop loop   exit         = GenLoop Nothing       exit loop
+-- | Loop with exit from the body.
 pattern Loop       loopIn exit loopOut = GenLoop (Just loopIn) exit loopOut
 
+-- | Make a workflow state from a `Name`.
 mkWfState :: Name -> WorkflowState
 mkWfState = fromPlace . makePlace
 
+-- | Make a workflow state from a `Place`.
 fromPlace :: Place -> WorkflowState
 fromPlace = unsafeWorkflowState . S.singleton
 
+-- | Generate a unique name.
 genName :: (MonadGen e m, Show e) => m Name
 genName = Name . show <$> gen
 
+-- | Generate a uniq workflow state.
 genWfState :: (MonadGen e m, Show e) => m WorkflowState
 genWfState = mkWfState <$> genName
 
+-- | Construct the list of transitions from a given `SafeWorkflowNet`.
 constructTransitions :: SafeWorkflowNet -> [Transition]
 constructTransitions = runGen . execWriterT . constructTransitionsM startState endState
 
+-- | Construct the list of transitions from a given `SafeWorkflowNet` `swf` and `start` and `end` states.
+-- The open-ended transitions of `swf` will be connected to the `start` and `end` states
+-- based on the stucture of `swf`.
 constructTransitionsM :: WorkflowState -> WorkflowState -> SafeWorkflowNet -> (WriterT [Transition] (Gen Integer)) ()
 constructTransitionsM start end (AND branches) = do
   inOuts <- forM (NE.toList branches) $ \br -> do
@@ -162,6 +178,7 @@ instance Arbitrary SafeWorkflowNet where
       xs <- replicateM k (genSWFNet (n `div` k))
       pure $ NE.fromList xs
 
+    -- | Sized `Maybe SafeWorkflowNet` generation
     mGenSWFNet :: Int -> QC.Gen (Maybe SafeWorkflowNet)
     mGenSWFNet n = resize n (arbitrary @(Maybe SafeWorkflowNet))
 
