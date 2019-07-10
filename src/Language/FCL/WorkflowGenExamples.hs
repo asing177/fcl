03da48f07aad8f8cda7858d9863fdd44786b9b4c
anabra <peter.d.podlovics@gmail.com>
2019-07-10 10:15:24 +0100

    Minor refactorings

--- src/Language/FCL/WorkflowGenExamples.hs
+++ src/Language/FCL/WorkflowGenExamples.hs
@@ -10,6 +10,10 @@ import Data.List.NonEmpty (NonEmpty(..))
 
 import Language.FCL.WorkflowGen
 
+-------------------------------------------------
+-- Basic building blocks of safe workflow nets --
+-------------------------------------------------
+
 namedBasicNets :: [(SafeWorkflowNet, [Char])]
 namedBasicNets = zip basicNets [ "atom"
                                , "XOR"
@@ -66,7 +70,9 @@ toLeftGenXOR = GenXOR Atom Atom Atom Atom Nothing (Just Atom)
 simpleGenXOR :: SafeWorkflowNet
 simpleGenXOR = GenXOR Atom Atom Atom Atom Nothing Nothing
 
-------------------------
+-------------------------------------------------------------------
+-- Already existing examples reproduced using safe workflow nets --
+-------------------------------------------------------------------
 
 namedExampleNets :: [(SafeWorkflowNet, [Char])]
 namedExampleNets = zip exampleNets [ "swap"
@@ -116,23 +122,21 @@ loanContractNet = Seq Atom (Loop Atom (XOR (Seq Atom (SimpleLoop Atom Atom)) Ato
 zcbNet :: SafeWorkflowNet
 zcbNet = XOR Atom (Loop Atom (Seq Atom Atom) (XOR Atom Atom))
 
--- can't express with current tools: communication between XOR branches needed (discrepancy -> nomination)
--- we need GenXOR
 gasForwardSimpleNet :: SafeWorkflowNet
 gasForwardSimpleNet = Seq Atom (XOR (GenXOR Atom (SimpleLoop Atom Atom) Atom Atom (Just Atom) Nothing) Atom)
 
 gasForwardNet :: SafeWorkflowNet
-gasForwardNet = Seq Atom (XOR (GenXOR Atom (SimpleLoop Atom Atom) Atom gasForwardNominationSubNet (Just Atom) Nothing) Atom)
+gasForwardNet = Seq Atom (XOR (GenXOR Atom (SimpleLoop Atom Atom) Atom gasForwardNominationSubNet (Just Atom) Nothing) Atom) where
 
-gasForwardNominationSubNet :: SafeWorkflowNet
-gasForwardNominationSubNet = SimpleLoop Atom (Seq (AND2 Atom (SimpleLoop Atom Atom)) (SimpleLoop Atom Atom))
+  gasForwardNominationSubNet :: SafeWorkflowNet
+  gasForwardNominationSubNet = SimpleLoop Atom (Seq (AND2 Atom (SimpleLoop Atom Atom)) (SimpleLoop Atom Atom))
 
 productNet :: SafeWorkflowNet
 productNet = Seq Atom (XOR (Seq Atom (SimpleLoop (XOR3 Atom Atom Atom) (XOR (Seq Atom (XOR3 (Seq Atom Atom) (Seq Atom Atom) (Seq Atom Atom))) (Loop Atom (Seq Atom Atom) (XOR Atom (Seq Atom Atom)))))) Atom)
 
----------------
-
--- some arbitrary SafeWorkflowNets
+---------------------------------------------------
+-- Some arbitrarily generated safe workflow nets --
+---------------------------------------------------
 
 namedArbitraryNets :: [(SafeWorkflowNet, [Char])]
 namedArbitraryNets = zipWith (\net id -> (net, "arbitrary-" <> show id)) arbitraryNets [0..]
@@ -149,8 +153,4 @@ arbitraryNets =
   , Seq {seqLhs = GenLoop {gLoopIn = Nothing, gLoopExit = GenLoop {gLoopIn = Just (AND {andBranches = Atom :| [Atom]}), gLoopExit = Atom, gLoopOut = Seq {seqLhs = Atom, seqRhs = Atom}}, gLoopOut = XOR {xorLhs = GenXOR {gXorLhsIn = Atom, gXorLhsOut = Atom, gXorRhsIn = Atom, gXorRhsOut = Atom, gXorMToRhs = Just Atom, gXorMToLhs = Just Atom}, xorRhs = Atom}}, seqRhs = XOR {xorLhs = Atom, xorRhs = XOR {xorLhs = XOR {xorLhs = Atom, xorRhs = XOR {xorLhs = Atom, xorRhs = Atom}}, xorRhs = XOR {xorLhs = Atom, xorRhs = XOR {xorLhs = Atom, xorRhs = Atom}}}}}
   , XOR {xorLhs = GenLoop {gLoopIn = Nothing, gLoopExit = XOR {xorLhs = AND {andBranches = Seq {seqLhs = Atom, seqRhs = Atom} :| [Seq {seqLhs = Atom, seqRhs = Atom}]}, xorRhs = Seq {seqLhs = XOR {xorLhs = Atom, xorRhs = XOR {xorLhs = Atom, xorRhs = Atom}}, seqRhs = GenLoop {gLoopIn = Nothing, gLoopExit = Atom, gLoopOut = Atom}}}, gLoopOut = AND {andBranches = XOR {xorLhs = XOR {xorLhs = Atom, xorRhs = XOR {xorLhs = Atom, xorRhs = Atom}}, xorRhs = Atom} :| [Seq {seqLhs = XOR {xorLhs = Atom, xorRhs = XOR {xorLhs = Atom, xorRhs = Atom}}, seqRhs = AND {andBranches = Atom :| [Atom,Atom]}}]}}, xorRhs = XOR {xorLhs = Seq {seqLhs = Atom, seqRhs = AND {andBranches = Atom :| [Atom,Atom,Atom]}}, xorRhs = XOR {xorLhs = AND {andBranches = Atom :| [Atom,Atom,Atom,Atom]}, xorRhs = XOR {xorLhs = GenXOR {gXorLhsIn = Atom, gXorLhsOut = Atom, gXorRhsIn = Atom, gXorRhsOut = Atom, gXorMToRhs = Just Atom, gXorMToLhs = Just Atom}, xorRhs = GenLoop {gLoopIn = Just Atom, gLoopExit = Atom, gLoopOut = Atom}}}}}
   , AND {andBranches = AND {andBranches = XOR {xorLhs = AND {andBranches = Atom :| [Atom]}, xorRhs = Atom} :| [XOR {xorLhs = Seq {seqLhs = AND {andBranches = Atom :| [Atom]}, seqRhs = XOR {xorLhs = Atom, xorRhs = XOR {xorLhs = Atom, xorRhs = Atom}}}, xorRhs = Atom}]} :| [XOR {xorLhs = GenXOR {gXorLhsIn = Atom, gXorLhsOut = Atom, gXorRhsIn = Atom, gXorRhsOut = Atom, gXorMToRhs = Just Atom, gXorMToLhs = Just Atom}, xorRhs = GenLoop {gLoopIn = Just (GenXOR {gXorLhsIn = Atom, gXorLhsOut = Atom, gXorRhsIn = Atom, gXorRhsOut = Atom, gXorMToRhs = Just Atom, gXorMToLhs = Nothing}), gLoopExit = XOR {xorLhs = Atom, xorRhs = XOR {xorLhs = Atom, xorRhs = Atom}}, gLoopOut = XOR {xorLhs = Atom, xorRhs = XOR {xorLhs = Atom, xorRhs = Atom}}}}]}
-  -- currently falsifiable
-  , AND {andBranches = Atom :| [Seq {seqLhs = GenXOR {gXorLhsIn = Atom, gXorLhsOut = Atom, gXorRhsIn = Atom, gXorRhsOut = Atom, gXorMToRhs = Nothing, gXorMToLhs = Nothing}, seqRhs = AND {andBranches = Atom :| [Atom]}}]}
   ]
-
-------------
