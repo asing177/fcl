48d7524bc04eabc2c52f856588c23211c002c064
Vilem Liepelt <v.liepelt@kent.ac.uk>
2019-07-17 12:01:58 +0100

    Simplify NonEmpty's Arbitrary instance

--- src/Language/FCL/Orphans.hs
+++ src/Language/FCL/Orphans.hs
@@ -9,23 +9,12 @@ import Data.List.NonEmpty (NonEmpty)
 import qualified Data.List.NonEmpty as NonEmpty
 import Data.Serialize (Serialize(..))
 import qualified Data.Serialize as Serialize
-import Test.QuickCheck (Arbitrary(..), NonEmptyList(..))
-
+import qualified Test.QuickCheck as QC
 
 instance Serialize a => Serialize (NonEmpty a) where
   get = NonEmpty.fromList <$> Serialize.get
   put = Serialize.put . NonEmpty.toList
 
-instance Arbitrary a => Arbitrary (NonEmpty a) where
-  arbitrary = do
-    xs <- getNonEmpty <$> arbitrary @(NonEmptyList a)
-    case xs of
-      (x:xs) -> return (x :| xs)
-      _      -> panic "QuickCheck generated an empty list for a NonEmptyList"
-
-  shrink (x :| xs)
-    | yss <- shrink (NonEmpty (x:xs))
-    = (flip map) yss $ \ys ->
-        case getNonEmpty ys of
-          (z:zs) -> (z :| zs)
-          _      -> panic "QuickCheck shrank a NonEmptyList into an empty list"
+instance QC.Arbitrary a => QC.Arbitrary (NonEmpty a) where
+  arbitrary = (:|) <$> QC.arbitrary <*> QC.arbitrary
+  shrink = QC.genericShrink
