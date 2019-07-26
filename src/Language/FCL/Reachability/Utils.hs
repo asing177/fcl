836ae902b8a1b6b83a5368a989099007ee1370e1
anabra <peter.d.podlovics@gmail.com>
2019-07-26 17:57:30 +0100

    Some refactorings is reachability utils

--- src/Language/FCL/Reachability/Utils.hs
+++ src/Language/FCL/Reachability/Utils.hs
@@ -10,6 +10,8 @@ import Data.Set (Set)
 import qualified Data.Set as S
 import qualified Data.Map as M
 
+import Language.FCL.Debug
+import Language.FCL.Pretty (ppr)
 import Language.FCL.AST (Place(..), Transition(..), WorkflowState(..), (\\), endState, isSubWorkflow, places, wfIntersection, wfUnion)
 import Language.FCL.Reachability.Definitions
 
@@ -28,24 +30,44 @@ applyTransition :: WorkflowState ->
                    Maybe (Either WFError WorkflowState)  -- global, local state
 applyTransition curr t@(Arrow src dst)
   | not (src `isSubWorkflow` curr) = Nothing -- can't fire transition
-  | src == endState                = Just . Left $ TransFromEnd t
-  | isNonEmpty sharedPlaces        = Just . Left $ NotOneBounded curr t sharedPlaces
+  | otherwise = Just (unsafeApplyTransition curr t)
+{-# INLINE applyTransition #-} -- very important for performance!!
+
+-- | Applies a transition to a given workflow state
+-- without checking whether the transition could fire at all.
+unsafeApplyTransition :: WorkflowState -> Transition -> Either WFError WorkflowState
+unsafeApplyTransition curr t@(Arrow src dst)
+  | not (src `isSubWorkflow` curr) = panic $ "unsafeApplyTransition: Can't fire '" <> (show $ ppr $ Debug t) <> "' from '" <> (show $ ppr $ Debug curr) <> "'"
+  | src == endState                = Left $ TransFromEnd t
+  | isNonEmpty sharedPlaces        = Left $ NotOneBounded curr t sharedPlaces
   | PlaceEnd `elem` places newState && newState /= endState
-    = Just . Left $ ImproperCompletion curr t newState
-  | otherwise = Just $ Right newState
+    = Left $ ImproperCompletion curr t newState
+  | otherwise = Right newState
     where
       sharedPlaces :: Set Place
       sharedPlaces = places $ (curr \\ src) `wfIntersection` dst
 
       newState :: WorkflowState
       newState = (curr \\ src) `wfUnion` dst
-{-# INLINE applyTransition #-} -- very important for performance!!
-
-unsafeApplyTransition :: WorkflowState -> Transition -> Maybe WorkflowState
-unsafeApplyTransition curr t@(Arrow src dst)
-  | not (src `isSubWorkflow` curr) = Nothing -- can't fire transition
-  | otherwise = Just $ (curr \\ src) `wfUnion` dst
 {-# INLINE unsafeApplyTransition #-}
+
+-- | Checks whether a given transition can be applied to a given state.
+-- Returns just an error if it is not applicable.
+checkTransition :: WorkflowState -> Transition -> Maybe WFError
+checkTransition curr t@(Arrow src dst)
+  | not (src `isSubWorkflow` curr) = Nothing
+  | src == endState                = Just $ TransFromEnd t
+  | isNonEmpty sharedPlaces        = Just $ NotOneBounded curr t sharedPlaces
+  | PlaceEnd `elem` places newState && newState /= endState
+    = Just $ ImproperCompletion curr t newState
+  | otherwise = Nothing
+    where
+      sharedPlaces :: Set Place
+      sharedPlaces = places $ (curr \\ src) `wfIntersection` dst
+
+      newState :: WorkflowState
+      newState = (curr \\ src) `wfUnion` dst
+{-# INLINE checkTransition #-}
 
 -- | Checks whether a given foldable structures is not empty.
 isNonEmpty :: Foldable f => f a -> Bool
@@ -60,7 +82,7 @@ isSingletonList _   = False
 isSingleton :: Foldable t => t a -> Bool
 isSingleton = isSingletonList . toList
 
-yell :: WFError -> GraphBuilderM s ()
+yell :: (MonadWriter (Set WFError, Set Transition) m) => WFError -> m ()
 yell err = tell (S.singleton err, mempty)
 {-# INLINE yell #-}
 
