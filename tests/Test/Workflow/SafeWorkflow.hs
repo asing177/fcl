d39652fd75a02e8f40e3c83c775d713f300bbf6a
anabra <peter.d.podlovics@gmail.com>
2019-08-10 20:12:12 +0100

    Added generalized acyclic control flow structure

--- tests/Test/Workflow/SafeWorkflow.hs
+++ tests/Test/Workflow/SafeWorkflow.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE DeriveGeneric, DeriveAnyClass #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE ViewPatterns #-}
 module Test.Workflow.SafeWorkflow
   ( SafeWorkflow(..)
   , pattern XOR3
@@ -15,8 +16,12 @@ import Protolude
 
 import Data.Maybe (fromJust)
 import Data.Set (Set(..))
+import Data.Map (Map(..))
 import Data.List.NonEmpty (NonEmpty(..))
+import Data.List (nub)
+
 import qualified Data.Set as S
+import qualified Data.Map as M
 import qualified Data.List.NonEmpty as NE
 
 import Control.Monad.Gen
@@ -34,20 +39,21 @@ import Language.FCL.Orphans()
 -- NOTE: GenXOR lhsIn lhsOut rhsIn rhsOut Nothing Nothing is equivalent to
 --       XOR (Seq lhsIn lhsOut) (Seq rhsIn rhsOut)
 
+data SWPlace = Entry  -- ^ Entry point to this sub-workflow.
+             | P Int  -- ^ Labelled intermediate place.
+             | Exit   -- ^ Exit point rom this sub-workflow.
+  deriving (Eq, Ord, Show, Generic, NFData, Arbitrary) -- TODO: remove Arbitrary
+
+data SWArrow = SWArrow SWPlace SWPlace
+  deriving (Eq, Ord, Show, Generic, NFData, Arbitrary) -- TODO: remove Arbitrary
+
 -- | Workflow nets that are sound by construction. We only allow these _safe_ workflow nets
 -- to be constructed in very specific ways in order to make soundness verification automatic.
 data SafeWorkflow
   -- TODO: should be List2 instead
+  -- TODO: branc in the comment
   -- | AND splitting into multiple workflows.
   = AND { andBranches :: NonEmpty SafeWorkflow    -- ^ At least one branc
-        }
-  -- | XOR splitting into two workflows.
-  | XOR { xorLhs :: SafeWorkflow                  -- ^ Left XOR branch
-        , xorRhs :: SafeWorkflow                  -- ^ Right XOR branch
-        }
-  -- |  Sequencing two workflow.
-  | Seq { seqLhs :: SafeWorkflow                  -- ^ First workflow
-        , seqRhs :: SafeWorkflow                  -- ^ Second workflow
         }
   -- | Looping construct with option to exit from both the body of the loop and the head of the loop.
   -- If the first half of the body is empty, we exit from head, otherwise exit from the body.
@@ -55,27 +61,33 @@ data SafeWorkflow
             , gLoopExit :: SafeWorkflow           -- ^ Exit from the loop
             , gLoopOut  :: SafeWorkflow           -- ^ Seconds half of the body of the loop
             }
-  -- | General XOR split with the possibility of moving between the branches.
-  | GenXOR { gXorLhsIn   :: SafeWorkflow          -- ^ First half of left-hand side
-           , gXorLhsOut  :: SafeWorkflow          -- ^ Second half of left-hand side
-           , gXorRhsIn   :: SafeWorkflow          -- ^ First half of right-hand side
-           , gXorRhsOut  :: SafeWorkflow          -- ^ Second half of right-hand side
-           , gXorMToRhs  :: (Maybe SafeWorkflow)  -- ^ Moving to the right-hand side from the left one
-           , gXorMToLhs  :: (Maybe SafeWorkflow)  -- ^ Moving to the left-hand side from the right one
+  -- | Generalized acyclic control-flow.
+  | GenACF { gACFMap :: Map SWArrow SafeWorkflow
            }
   -- | Atom representing a single transition.
   | Atom
   deriving (Eq, Ord, Show, Generic, NFData)
 
-{-# COMPLETE XOR, AND, Seq, SimpleLoop, Loop, GenXOR #-}
+-- | XOR with two branches.
+pattern XOR lhs rhs <- GenACF (M.toList -> [(SWArrow Entry Exit, lhs), (SWArrow Entry Exit, rhs)])
+  where XOR lhs rhs  = GenACF (M.fromList  [(SWArrow Entry Exit, lhs), (SWArrow Entry Exit, rhs)])
 -- | XOR with three branches.
 pattern XOR3 a b c   = XOR a (XOR b c)
+-- | XOR with unidirectional communication between branches.
+pattern GenXOR lhsIn lhsOut rhsIn rhsOut lhsToRhs <- GenACF (M.toList -> [(SWArrow Entry (P 1), lhsIn), (SWArrow (P 1) Exit, lhsOut), (SWArrow Entry (P 2), rhsIn), (SWArrow (P 2) Exit, rhsOut), (SWArrow (P 1) (P 2), lhsToRhs)])
+  where GenXOR lhsIn lhsOut rhsIn rhsOut lhsToRhs =  GenACF (M.fromList  [(SWArrow Entry (P 1), lhsIn), (SWArrow (P 1) Exit, lhsOut), (SWArrow Entry (P 2), rhsIn), (SWArrow (P 2) Exit, rhsOut), (SWArrow (P 1) (P 2), lhsToRhs)])
 -- | AND with two branches.
+pattern Seq lhs rhs <- GenACF (M.toList -> [(SWArrow Entry (P 1), lhs), (SWArrow (P 1) Exit, rhs)])
+  where Seq lhs rhs =  GenACF (M.fromList  [(SWArrow Entry (P 1), lhs), (SWArrow (P 1) Exit, rhs)])
 pattern AND2 lhs rhs = AND (lhs :| [rhs])
 -- | Simple loop with exit only from the head.
 pattern SimpleLoop loop   exit         = GenLoop Nothing       exit loop
 -- | Loop with exit from the body.
 pattern Loop       loopIn exit loopOut = GenLoop (Just loopIn) exit loopOut
+
+isIntermediate :: SWPlace -> Bool
+isIntermediate (P _) = True
+isIntermediate _     = False
 
 -- | Make a workflow state from a `Name`.
 singletonWfState :: Name -> WorkflowState
@@ -105,9 +117,6 @@ constructTransitionsM start end (AND branches) = do
     pure (inSt, outSt)
   let (ins, outs) = unzip inOuts
   tell [Arrow start (mconcat ins), Arrow (mconcat outs) end]
-constructTransitionsM start end (XOR lhs rhs) = do
-  constructTransitionsM start end lhs
-  constructTransitionsM start end rhs
 constructTransitionsM start end (Seq lhs rhs) = do
   inBetween <- genWfState
   constructTransitionsM start inBetween lhs
@@ -120,30 +129,35 @@ constructTransitionsM start end (Loop gLoopIn exit gLoopOut) = do
   constructTransitionsM start inBetween gLoopIn
   constructTransitionsM inBetween end   exit
   constructTransitionsM inBetween start gLoopOut
-constructTransitionsM start end GenXOR{..} = do
-  lhsP <- genWfState
-  rhsP <- genWfState
-  constructTransitionsM start lhsP gXorLhsIn
-  constructTransitionsM lhsP  end  gXorLhsOut
-  constructTransitionsM start rhsP gXorRhsIn
-  constructTransitionsM rhsP  end  gXorRhsOut
-  when (isJust gXorMToRhs) $
-    constructTransitionsM lhsP rhsP (fromJust gXorMToRhs)
-  when (isJust gXorMToLhs) $
-    constructTransitionsM rhsP lhsP (fromJust gXorMToLhs)
+constructTransitionsM start end (GenACF acf) = do
+  let acfList = M.toList acf
+      acfPlaces = S.fromList
+                . filter isIntermediate
+                . concatMap (\(SWArrow x y) -> [x,y])
+                . map fst
+                $ acfList
+  stateMap <- sequence $ M.fromSet (const genWfState) acfPlaces
+  let getState :: SWPlace -> WorkflowState
+      getState Entry = start
+      getState Exit  = end
+      getState p     = fromMaybe (panic $ "constructTransitionsM: Place " <> show p <> " is not present in state map.") $ M.lookup p stateMap
+  forM_ acfList $ \(SWArrow from to, swf) -> do
+    let from' = getState from
+        to'   = getState to
+    constructTransitionsM from' to' swf
 constructTransitionsM start end Atom = do
   tell [Arrow start end]
 
+-- TODO: add genACF generation
 instance Arbitrary SafeWorkflow where
   arbitrary = sized genSWFNet where
     -- | Sized `SafeWorkflow generation
     genSWFNet :: Int -> QC.Gen SafeWorkflow
     genSWFNet n |          n <= 1 = atom
-    genSWFNet n | 1 < n && n <= 2 = complexity 2
-    genSWFNet n | 2 < n && n <= 3 = complexity 3
-    genSWFNet n | 3 < n && n <= 4 = complexity 4
-    genSWFNet n | 4 < n && n <= 5 = complexity 5
-    genSWFNet n | 5 < n           = oneof (allComplexities n)
+    genSWFNet n | 1 < n && n <= 2 = maxComplexity 2
+    genSWFNet n | 2 < n && n <= 3 = maxComplexity 3
+    genSWFNet n | 3 < n && n <= 4 = maxComplexity 4
+    genSWFNet n | 4 < n           = maxComplexity 5
     genSWFNet n = panic $ "Negative value for SafeWorkflow generation: " <> show n
 
     partitionThen :: Int -> Int -> ([Int] -> QC.Gen a) -> QC.Gen a
@@ -155,11 +169,11 @@ instance Arbitrary SafeWorkflow where
           aPartition <- elements partitions
           g aPartition
 
-    complexity :: Int -> QC.Gen SafeWorkflow
-    complexity n = oneof $ take n (allComplexities n)
+    maxComplexity :: Int -> QC.Gen SafeWorkflow
+    maxComplexity n = oneof $ take n (allComplexities n)
 
     allComplexities :: Int -> [QC.Gen SafeWorkflow]
-    allComplexities n = [ atom, complexity2 n, complexity3 n, complexity4 n, complexity5 n, complexity6 n ]
+    allComplexities n = [ atom, complexity2 n, complexity3 n, complexity4 n, complexity5 n ]
 
     atom :: QC.Gen SafeWorkflow
     atom = pure Atom
@@ -188,12 +202,6 @@ instance Arbitrary SafeWorkflow where
       case partition of
         [k1,k2,k3,k4] -> oneof
           [ AND <$> someSWFNets (n-2) 2
-          , GenXOR <$> genSWFNet k1
-                   <*> genSWFNet k2
-                   <*> genSWFNet k3
-                   <*> genSWFNet k4
-                   <*> pure Nothing
-                   <*> pure Nothing
           ]
         _ -> panic $ show n <> " was not partitioned into 4 components"
 
@@ -206,29 +214,9 @@ instance Arbitrary SafeWorkflow where
                    <*> genSWFNet k2
                    <*> genSWFNet k3
                    <*> genSWFNet k4
-                   <*> mGenSWFNet k5
-                   <*> pure Nothing
-          , GenXOR <$> genSWFNet k1
-                   <*> genSWFNet k2
-                   <*> genSWFNet k3
-                   <*> genSWFNet k4
-                   <*> pure Nothing
-                   <*> mGenSWFNet k5
+                   <*> genSWFNet k5
           ]
         _ -> panic $ show n <> " was not partitioned into 5 components"
-
-    complexity6 :: Int -> QC.Gen SafeWorkflow
-    complexity6 n = partitionThen n 6 $ \partition -> do
-      case partition of
-        [k1,k2,k3,k4,k5,k6] -> oneof
-          [ GenXOR <$> genSWFNet k1
-                   <*> genSWFNet k2
-                   <*> genSWFNet k3
-                   <*> genSWFNet k4
-                   <*> mGenSWFNet k5
-                   <*> mGenSWFNet k6
-          ]
-        _ -> panic $ show n <> " was not partitioned into 6 components"
 
     -- | Generates `SafeWorkflow`s of summed size `n`.
     someSWFNets :: Int -> Int -> QC.Gen (NonEmpty SafeWorkflow)
@@ -236,10 +224,6 @@ instance Arbitrary SafeWorkflow where
       partitionThen n k $ \ps -> do
         xs <- mapM genSWFNet ps
         pure $ NE.fromList xs
-
-    -- | Sized `Maybe SafeWorkflow` generation
-    mGenSWFNet :: Int -> QC.Gen (Maybe SafeWorkflow)
-    mGenSWFNet n = resize n (arbitrary @(Maybe SafeWorkflow))
 
     partitionInt d = go d d where
       go _  0  = [[]]
