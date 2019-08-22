49d3d8ff730006b4e8458f2c2af96359aef6c9e2
anabra <peter.d.podlovics@gmail.com>
2019-08-22 16:58:22 +0100

    Added Graphviz instance for SafeWorkflow

--- src/Language/FCL/SafeWorkflow.hs
+++ src/Language/FCL/SafeWorkflow.hs
@@ -5,6 +5,8 @@
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE InstanceSigs #-}
 module Language.FCL.SafeWorkflow
   ( SafeWorkflow(Atom, AND, GenLoop)
 
@@ -61,8 +63,12 @@ import Test.QuickCheck hiding (Gen)
 import qualified Test.QuickCheck as QC
 
 import Language.FCL.AST
-import Language.FCL.Pretty (ppr, (<+>), Pretty)
+import Language.FCL.Pretty (Pretty, ppr, prettyPrint, (<+>), hsep)
 import Language.FCL.Orphans()
+import Language.FCL.Graphviz hiding (AnnotatedTransition)
+import Language.FCL.Analysis (inferStaticWorkflowStates)
+
+import qualified Language.FCL.Graphviz as GV
 
 -- NOTE: can't return back to different places from a loop (eg.: novation.s)
 -- NOTE: syncronisation points are always singleton states (places) (eg.: novation.s)
@@ -114,6 +120,10 @@ data SafeWorkflow a
 
 {-# COMPLETE AND, GenLoop,          ACF, Atom #-}
 {-# COMPLETE AND, SimpleLoop, Loop, ACF, Atom #-}
+
+-----------------------------------------------------------------
+-- Pattern synonyms, selector functions and smart constructors --
+-----------------------------------------------------------------
 
 -- | XOR with two branches.
 pattern XOR :: SafeWorkflow a -> SafeWorkflow a -> SafeWorkflow a
@@ -223,6 +233,10 @@ unsafeMkACF :: ACFMap a -> SafeWorkflow a
 unsafeMkACF = either mkError identity . mkACF where
   mkError arrows = panic $ "unsafeMkACF: The following arrows are invalid: " <> show arrows
 
+---------------------------------------------------
+-- Constructing transitions from @SafeWorkflow@s --
+---------------------------------------------------
+
 -- | Make a workflow state from a `Name`.
 singletonWfState :: Name -> WorkflowState
 singletonWfState = unsafeWorkflowState . S.singleton . makePlace
@@ -294,6 +308,10 @@ constructAnnTransitionsM start end (ACF (M.toList -> acfList)) = do
     mapM_ (constructAnnTransitionsM from' to') swfs
 constructAnnTransitionsM start end Atom{..} = do
   tell [AnnTransition atomAnnot $ Arrow start end]
+
+-------------------------
+-- Arbitrary instances --
+-------------------------
 
 instance Pretty ACFPlace where
   ppr Entry = "Entry"
@@ -525,3 +543,42 @@ boundError lo hi = show $ "between: Can't generate new place in between" <+> ppr
 -- Used for determining whetehr two ACF places can accomodate an additional place in-between.
 hasIncorrectBounds :: Int -> Int -> Bool
 hasIncorrectBounds lo hi = abs (hi - lo) < 2
+
+------------------------
+-- Graphviz instances --
+------------------------
+
+instance Show a => DisplayableWorkflow (SafeWorkflow a) where
+  -- | Annotated transition.
+  data AnnotatedTransition (SafeWorkflow a)
+    = AnnTr { getAnnTr :: AnnotatedTransition a
+            }
+
+  renderTransitionNode :: GV.AnnotatedTransition (SafeWorkflow a) -> Graphviz
+  renderTransitionNode (getAnnot . getAnnTr -> ann) = mconcat
+    [ show ann
+    , "[label=<"
+    , "<FONT POINT-SIZE=\"16\">" <> show ann <> "</FONT>"
+    , "<FONT POINT-SIZE=\"10\" COLOR=\"blue\"> "
+    , "</FONT>"
+    , ">"
+    , "shape=box; fontname=\"Arial\"; style=filled; color=black; fillcolor=gray75;]"
+    ]
+
+  renderTransitionArrows :: GV.AnnotatedTransition (SafeWorkflow a) -> Graphviz
+  renderTransitionArrows (getAnnTr -> AnnTransition ann (Arrow src dst)) = prettyPrint $ hsep
+    [ ppr src
+    , "->"
+    , ppr (show ann :: Text)
+    , ";"
+    , ppr (show ann :: Text)
+    , "->"
+    , ppr dst
+    ]
+
+  annotatedTransitions :: SafeWorkflow a -> [GV.AnnotatedTransition (SafeWorkflow a)]
+  annotatedTransitions = map AnnTr . constructAnnTransitions
+
+  staticWorkflowStates :: SafeWorkflow a -> Set WorkflowState
+  staticWorkflowStates = inferStaticWorkflowStates
+                       . constructTransitions
