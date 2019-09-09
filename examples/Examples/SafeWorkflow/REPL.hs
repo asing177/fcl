84b8787c5f41a7ae670b08f73bd7a2798dd36fe2
anabra <peter.d.podlovics@gmail.com>
2019-09-09 17:57:47 +0100

    Separated REPL examples into a new executable

---
+++ examples/Examples/SafeWorkflow/REPL.hs
@@ -0,0 +1,137 @@
+{-# LANGUAGE OverloadedLists #-}
+module Examples.SafeWorkflow.REPL where
+
+import Protolude hiding (Type, sequence, option)
+
+import qualified Data.ByteString as BS
+
+import Numeric.Lossless.Decimal (Decimal(..))
+
+import Language.FCL.AST
+import Language.FCL.Address (Address(..))
+import Language.FCL.SafeWorkflow.Editable (noLoc)
+import Language.FCL.SafeWorkflow.REPL
+
+xLTFive :: Expr
+xLTFive = EBinOp (noLoc Lesser)
+  (noLoc $ EVar $ noLoc $ "x")
+  (noLoc $ ELit $ noLoc $ LNum $ Decimal 0 5)
+
+xEQZero :: Expr
+xEQZero = EBinOp (noLoc Equal)
+  (noLoc $ EVar $ noLoc $ "x")
+  (noLoc $ ELit $ noLoc $ LNum $ Decimal 0 0)
+
+xAssign :: Integer -> Expr
+xAssign n = EAssign ["x"]
+  (noLoc $ ELit $ noLoc $ LNum $ Decimal 0 n)
+
+mkArg :: Type -> Name -> Arg
+mkArg ty name = Arg ty (noLoc name)
+
+account :: Word8 -> Expr
+account = ELit . noLoc . LAccount . Address . BS.singleton
+
+decimal :: Integer -> Type
+decimal = TNum . NPDecimalPlaces
+
+simpleWhiteBoardExample3 :: SWREPLM ()
+simpleWhiteBoardExample3 = do
+  parallel 1
+    "split" (xAssign 10)
+    "join"  (xAssign 20)
+    [ ("lhsIn", "lhsOut")
+    , ("rhsIn", "rhsOut")
+    ]
+  finish 1 "t1" $ xAssign 1
+  conditional 2 xLTFive
+  finish 8 "t2" $ xAssign 2
+  stayOrContinue 9 xEQZero
+  finish 11 "t2" $ xAssign 3
+  finish 12 "t2" $ xAssign 4
+
+conditionalInConditionalLeft :: SWREPLM ()
+conditionalInConditionalLeft = do
+  conditional 1 xLTFive
+  conditional 1 xEQZero
+  finish 2 "t1" $ xAssign 1
+  finish 3 "t1" $ xAssign 2
+  finish 4 "t1" $ xAssign 3
+
+conditionalInConditionalRight :: SWREPLM ()
+conditionalInConditionalRight = do
+  conditional 1 xLTFive
+  finish 1 "t1" $ xAssign 1
+  conditional 2 xEQZero
+  finish 4 "t1" $ xAssign 2
+  finish 5 "t1" $ xAssign 3
+
+seqInConditionalLeft :: SWREPLM ()
+seqInConditionalLeft = do
+  conditional 1 xLTFive
+  sequence 1 "inbetween"
+  finish 11 "t1" $ xAssign 1
+  finish 12 "t2" $ xAssign 2
+  finish 2  "t1" $ xAssign 3
+
+seqInConditionalRight :: SWREPLM ()
+seqInConditionalRight = do
+  conditional 1 xLTFive
+  sequence 2 "inbetween"
+  finish 21 "t1" $ xAssign 1
+  finish 22 "t2" $ xAssign 2
+  finish 1  "t1" $ xAssign 3
+
+simpleOption :: SWREPLM ()
+simpleOption = do
+  addGlobalWithDefault "alice" TAccount (account 100)
+  addGlobalWithDefault "bob"   TAccount (account 101)
+  option 1
+  finish 1 "t1" $ xAssign 1
+  finish 2 "t2" $ xAssign 2
+  addPrecondition "t1" PrecRoles (role "alice")
+  addPrecondition "t2" PrecRoles (role "bob")
+  addArgs "t1" [mkArg TBool "b"]
+
+-- FIXME: you shouldn't be able to do undeterministic branching
+-- inside a deterministic IF condition
+-- TODO: disallow this
+optionInConditionalLeft :: SWREPLM ()
+optionInConditionalLeft = do
+  conditional 1 xLTFive
+  option 1
+  finish 11 "t1" $ xAssign 1
+  finish 12 "t2" $ xAssign 2
+  finish 2  "t1" $ xAssign 3
+
+loanContract :: SWREPLM ()
+loanContract = do
+  addGlobalSimple "principle"     $ decimal 2
+  addGlobalSimple "interest_rate" $ decimal 2
+  addGlobalSimple "currency"      $ TAsset $ decimal 2
+  addGlobalSimple "borrower"        TAccount
+  addGlobalSimple "lender"          TAccount
+  addGlobalSimple "loan_contract"   TText
+
+  sequence 1 "negotiate_terms"
+  finish 1 "propose_contract" $ ENoOp
+  loopOrContinue 2 ENoOp "make_decision"
+  finish 4 "propose_terms" $ ENoOp
+  finish 6 "revise" $ ENoOp
+  option 5
+  finish 2 "reject" $ ENoOp
+  sequence 1 "signed"
+  finish 1 "sign" $ ENoOp
+  sequence 2 "contract_active"
+  finish 1 "loan_start" $ ENoOp
+  stayOrContinue 2 ENoOp
+  finish 15 "pay_interest" $ ENoOp
+  finish 14 "payback" $ ENoOp
+
+  addRole "propose_terms" "lender"
+  addRole "sign"          "borrower"
+  addRole "revise"        "borrower"
+  addRole "reject"        "borrower"
+  addRole "loan_start"    "lender"
+  addRole "loan_interest" "borrower"
+  addRole "payback"       "borrower"
--- src/Language/FCL/SafeWorkflow/REPL.hs
+++ src/Language/FCL/SafeWorkflow/REPL.hs
@@ -10,7 +10,6 @@ import Data.List.List2 (List2(..))
 import Data.Monoid (Dual(..))
 
 import qualified Data.Map as M
-import qualified Data.ByteString as BS
 
 import Control.Monad.Gen
 import Control.Monad.RWS hiding (sequence)
@@ -18,10 +17,7 @@ import Control.Monad.RWS hiding (sequence)
 import System.FilePath (FilePath, (</>))
 import System.Directory (createDirectoryIfMissing)
 
-import Numeric.Lossless.Decimal (Decimal(..))
-
 import Language.FCL.AST
-import Language.FCL.Address (Address(..))
 import Language.FCL.Graphviz (workflowWriteSVG)
 import Language.FCL.SafeWorkflow.Editable
 import Language.FCL.SafeWorkflow.CodeGen (CGInfo(..), codeGenScript, fromPreconds, fromArgs)
@@ -82,6 +78,23 @@ loggedModify transform = do
   let sw' = transform editableWorkflow
   tell $ Dual [sw']
   put $ s { editableWorkflow = sw' }
+
+printSW :: FilePath -> EditableSW -> IO ()
+printSW path = workflowWriteSVG path . prettify
+
+execCodeGen :: SWREPLM a -> Script
+execCodeGen = codeGenScript
+            . snd3
+            . runSWREPLPure defaultOpts
+  where
+    snd3 (_,x,_) = x
+
+execCodeGenThenPrintScript :: SWREPLM a -> IO ()
+execCodeGenThenPrintScript = putStr . prettyPrint . execCodeGen where
+
+-----------------------------
+-- Control-flow primitives --
+-----------------------------
 
 noCode :: CGMetadata
 noCode = CGMetadata Nothing Nothing
@@ -195,6 +208,16 @@ acf
   -> SWREPLM ()
 acf = panic "not implemented"
 
+----------------------------------
+-- Method annotation primitives --
+----------------------------------
+
+neg :: Expr -> Expr
+neg = EUnOp (noLoc Not) . noLoc
+
+role :: Name -> Expr
+role = EVar . noLoc
+
 initMethodAnnots
   :: Name
   -> SWREPLM ()
@@ -260,153 +283,3 @@ addGlobalWithDefault
   -> SWREPLM ()
 addGlobalWithDefault name ty def = addGlobal name ty [] (Just def)
 
-printSW :: FilePath -> EditableSW -> IO ()
-printSW path = workflowWriteSVG path . prettify
-
-execCodeGen :: SWREPLM a -> Script
-execCodeGen = codeGenScript
-            . snd3
-            . runSWREPLPure defaultOpts
-  where
-    snd3 (_,x,_) = x
-
-execCodeGenThenPrintScript :: SWREPLM a -> IO ()
-execCodeGenThenPrintScript = putStr . prettyPrint . execCodeGen where
-
-trueCond :: Expr
-trueCond = ELit . noLoc . LBool $ True
-
-zeroLTOne :: Expr
-zeroLTOne = EBinOp (noLoc Lesser)
-  (noLoc $ ELit $ noLoc $ LNum $ Decimal 0 0)
-  (noLoc $ ELit $ noLoc $ LNum $ Decimal 0 1)
-
-xLTFive :: Expr
-xLTFive = EBinOp (noLoc Lesser)
-  (noLoc $ EVar $ noLoc $ "x")
-  (noLoc $ ELit $ noLoc $ LNum $ Decimal 0 5)
-
-xEQZero :: Expr
-xEQZero = EBinOp (noLoc Equal)
-  (noLoc $ EVar $ noLoc $ "x")
-  (noLoc $ ELit $ noLoc $ LNum $ Decimal 0 0)
-
-xAssign :: Integer -> Expr
-xAssign n = EAssign ["x"]
-  (noLoc $ ELit $ noLoc $ LNum $ Decimal 0 n)
-
-neg :: Expr -> Expr
-neg = EUnOp (noLoc Not) . noLoc
-
-role :: Name -> Expr
-role = EVar . noLoc
-
-mkArg :: Type -> Name -> Arg
-mkArg ty name = Arg ty (noLoc name)
-
-account :: Word8 -> Expr
-account = ELit . noLoc . LAccount . Address . BS.singleton
-
-decimal :: Integer -> Type
-decimal = TNum . NPDecimalPlaces
-
-simpleWhiteBoardExample3 :: SWREPLM ()
-simpleWhiteBoardExample3 = do
-  parallel 1
-    "split" (xAssign 10)
-    "join"  (xAssign 20)
-    [ ("lhsIn", "lhsOut")
-    , ("rhsIn", "rhsOut")
-    ]
-  finish 1 "t1" $ xAssign 1
-  conditional 2 xLTFive
-  finish 8 "t2" $ xAssign 2
-  stayOrContinue 9 xEQZero
-  finish 11 "t2" $ xAssign 3
-  finish 12 "t2" $ xAssign 4
-
-conditionalInConditionalLeft :: SWREPLM ()
-conditionalInConditionalLeft = do
-  conditional 1 xLTFive
-  conditional 1 xEQZero
-  finish 2 "t1" $ xAssign 1
-  finish 3 "t1" $ xAssign 2
-  finish 4 "t1" $ xAssign 3
-
-conditionalInConditionalRight :: SWREPLM ()
-conditionalInConditionalRight = do
-  conditional 1 xLTFive
-  finish 1 "t1" $ xAssign 1
-  conditional 2 xEQZero
-  finish 4 "t1" $ xAssign 2
-  finish 5 "t1" $ xAssign 3
-
-seqInConditionalLeft :: SWREPLM ()
-seqInConditionalLeft = do
-  conditional 1 xLTFive
-  sequence 1 "inbetween"
-  finish 11 "t1" $ xAssign 1
-  finish 12 "t2" $ xAssign 2
-  finish 2  "t1" $ xAssign 3
-
-seqInConditionalRight :: SWREPLM ()
-seqInConditionalRight = do
-  conditional 1 xLTFive
-  sequence 2 "inbetween"
-  finish 21 "t1" $ xAssign 1
-  finish 22 "t2" $ xAssign 2
-  finish 1  "t1" $ xAssign 3
-
-simpleOption :: SWREPLM ()
-simpleOption = do
-  addGlobalWithDefault "alice" TAccount (account 100)
-  addGlobalWithDefault "bob"   TAccount (account 101)
-  option 1
-  finish 1 "t1" $ xAssign 1
-  finish 2 "t2" $ xAssign 2
-  addPrecondition "t1" PrecRoles (role "alice")
-  addPrecondition "t2" PrecRoles (role "bob")
-  addArgs "t1" [mkArg TBool "b"]
-
--- FIXME: you shouldn't be able to do undeterministic branching
--- inside a deterministic IF condition
--- TODO: disallow this
-optionInConditionalLeft :: SWREPLM ()
-optionInConditionalLeft = do
-  conditional 1 xLTFive
-  option 1
-  finish 11 "t1" $ xAssign 1
-  finish 12 "t2" $ xAssign 2
-  finish 2  "t1" $ xAssign 3
-
-loanContract :: SWREPLM ()
-loanContract = do
-  addGlobalSimple "principle"     $ decimal 2
-  addGlobalSimple "interest_rate" $ decimal 2
-  addGlobalSimple "currency"      $ TAsset $ decimal 2
-  addGlobalSimple "borrower"        TAccount
-  addGlobalSimple "lender"          TAccount
-  addGlobalSimple "loan_contract"   TText
-
-  sequence 1 "negotiate_terms"
-  finish 1 "propose_contract" $ ENoOp
-  loopOrContinue 2 ENoOp "make_decision"
-  finish 4 "propose_terms" $ ENoOp
-  finish 6 "revise" $ ENoOp
-  option 5
-  finish 2 "reject" $ ENoOp
-  sequence 1 "signed"
-  finish 1 "sign" $ ENoOp
-  sequence 2 "contract_active"
-  finish 1 "loan_start" $ ENoOp
-  stayOrContinue 2 ENoOp
-  finish 15 "pay_interest" $ ENoOp
-  finish 14 "payback" $ ENoOp
-
-  addRole "propose_terms" "lender"
-  addRole "sign"          "borrower"
-  addRole "revise"        "borrower"
-  addRole "reject"        "borrower"
-  addRole "loan_start"    "lender"
-  addRole "loan_interest" "borrower"
-  addRole "payback"       "borrower"
