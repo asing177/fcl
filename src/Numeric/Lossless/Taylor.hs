8e38a7b4819fa6c0b3c0460fdc9b740ae1d956fc
acentelles <centelles.alberto@gmail.com>
2019-10-08 14:27:41 +0100

    Use Real constraint

--- src/Numeric/Lossless/Taylor.hs
+++ src/Numeric/Lossless/Taylor.hs
@@ -2,7 +2,15 @@
 module: Numeric.Lossless.Taylor
 description: Real functions approximated by Taylor series.
 -}
-module Numeric.Lossless.Taylor (expon, ln, pow) where
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ViewPatterns #-}
+module Numeric.Lossless.Taylor
+  ( expon
+  , ln
+  , pow
+  , calculateTerms
+  , lagrangeFormula
+  ) where
 
 import Protolude
 
@@ -18,21 +26,25 @@ ln p x
 pow :: (Fractional b, Ord b) => Int -> b -> b -> Maybe b
 pow p x y = expon p <$> liftA2 (*) (ln p x) (Just y)
 
+conv :: Real a => a -> Double
+conv = realToFrac
+
 -- | Lagrange formula for the particular case of b^x = e^{x \log b} at a = 0
 -- R_n(x) = \dfrac{f^{(n+1)}(c)}{(n+1)!} (x-a)^{n+1}
 -- As f=e^{x \log b} then f^{(n+1)}(x) = \log^n b e^{x \log b}
-lagrangeFormula :: (Floating a) => a -> a -> a -> Int -> a
-lagrangeFormula b x c n = (log b)^n * exp (c * (log b)) * (x ^ (n + 1)) / fromIntegral (product [1..(n+1)])
+-- These formulas should be valid latex. Check them on https://arachnoid.com/latex/
+lagrangeFormula :: (Real a) => a -> a -> a -> Int -> Double
+lagrangeFormula (conv -> b) (conv -> x) (conv -> c) n = (log b)^n * exp (c * (log b)) * (x ^ (n + 1)) / fromIntegral (product [1..(n+1)])
 
 -- | Given an error bound, calculate number of terms
-calculateTerms :: (Floating a, Ord a) => a -> a -> a -> Int
+calculateTerms :: (Show a, Real a, Ord a) => a -> a -> a -> Int
 calculateTerms bound b x = go 0
   where
-    go n = if lagrangeFormula b x c n <= bound then n else go (n+1)
+    go n = if lagrangeFormula b x c n <= conv bound then n else go (n+1)
     c = if b < 0
         then panic "Base b in b^x cannot be lower than 0"
         -- When b > 0. b^x is monotonic
         else if b < 1
-             then if x < a then a else x
-             else if x < a then x else a
+             then if x < a then x else a
+             else if x < a then a else x
     a = 0 -- a=0 in our implementation of Taylor series
