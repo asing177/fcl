95eeb48a11a721e2d6f7148121e2a4af27c60144
acentelles <centelles.alberto@gmail.com>
2019-10-08 12:15:02 +0100

    Implement lagrange formula and calculate terms given error bound

--- src/Numeric/Lossless/Taylor.hs
+++ src/Numeric/Lossless/Taylor.hs
@@ -6,6 +6,7 @@ module Numeric.Lossless.Taylor (expon, ln, pow) where
 
 import Protolude
 
+-- | Taylor series for the exponential function e^x at a = 0 is \sum_{n=0}^{\infty}x^n/n!
 expon :: Fractional a => Int -> a -> a
 expon p x = sum $ take p [x^n / fromIntegral (product [1..n]) | n <- [0..]]
 
@@ -16,3 +17,22 @@ ln p x
 
 pow :: (Fractional b, Ord b) => Int -> b -> b -> Maybe b
 pow p x y = expon p <$> liftA2 (*) (ln p x) (Just y)
+
+-- | Lagrange formula for the particular case of b^x = e^{x \log b} at a = 0
+-- R_n(x) = \dfrac{f^{(n+1)}(c)}{(n+1)!} (x-a)^{n+1}
+-- As f=e^{x \log b} then f^{(n+1)}(x) = \log^n b e^{x \log b}
+lagrangeFormula :: (Floating a) => a -> a -> a -> Int -> a
+lagrangeFormula b x c n = (log b)^n * exp (c * (log b)) * (x ^ (n + 1)) / fromIntegral (product [1..(n+1)])
+
+-- | Given an error bound, calculate number of terms
+calculateTerms :: (Floating a, Ord a) => a -> a -> a -> Int
+calculateTerms bound b x = go 0
+  where
+    go n = if lagrangeFormula b x c n <= bound then n else go (n+1)
+    c = if b < 0
+        then panic "Base b in b^x cannot be lower than 0"
+        -- When b > 0. b^x is monotonic
+        else if b < 1
+             then if x < a then a else x
+             else if x < a then x else a
+    a = 0 -- a=0 in our implementation of Taylor series
