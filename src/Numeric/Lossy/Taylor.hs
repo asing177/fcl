76979dbda00006d04e5ebdff26b4123e419d5339
Vilem Liepelt <mail@vilem.net>
2019-10-08 15:15:56 +0100

    [wip] Sketch expon, ln, pow

--- src/Numeric/Lossy/Taylor.hs
+++ src/Numeric/Lossy/Taylor.hs
@@ -16,16 +16,19 @@ import Protolude
 
 -- | Taylor series for the exponential function e^x at a = 0 is \sum_{n=0}^{\infty}x^n/n!
 expon :: Fractional a => Int -> a -> a
-expon p x = sum $ take p [x^n / fromIntegral (product [1..n]) | n <- [0..]]
+expon bound x = sum $ take k [x^n / fromIntegral (product [1..n]) | n <- [0..]]
+  where
+    k = calculateTermsExp bound x
 
-ln :: Ord a => Fractional a => Int -> a -> Maybe a
-ln p x
+ln :: Real b => Ord a => Fractional a => b -> a -> Maybe a
+ln bound x
   | x > 0 = Just $ 2 * (sum $ take p $ [recip (fromIntegral n) * ((x-1)/(x+1))^n | n <- [1,3..]])
   | otherwise = Nothing
+  where
+    k = calculateTermsLog bound x
 
-pow :: (Fractional b, Ord b) => Int -> b -> b -> Maybe b
-pow p x y = expon p <$> liftA2 (*) (ln p x) (Just y)
-
+pow :: (Real b, Fractional a, Ord a) => b -> a -> a -> Maybe a
+pow bound b x = expon bound <$> (liftA2 (*) (ln bound b) (Just x))
 
 -- | Lagrange formula for the particular case of \(b^x = e^{x \log b} at a = 0\).
 -- \[
@@ -38,8 +41,8 @@ lagrangeFormula (realToFrac -> b) (realToFrac -> x) (realToFrac -> c) n
   = (log b)^n * exp (c * (log b)) * (x ^ (n + 1)) / fromIntegral (product [1..(n+1)])
 
 -- | Given an error bound, calculate number of terms
-calculateTerms :: (Real a) => a -> a -> a -> Int
-calculateTerms bound b x = go 0
+calculateTermsExp :: (Real a) => a -> a -> a -> Int
+calculateTermsExp bound b x = go 0
   where
     go n = if lagrangeFormula b x c n <= realToFrac bound then n else go (n+1)
     c = if b < 0
@@ -49,3 +52,6 @@ calculateTerms bound b x = go 0
              then if x < a then x else a
              else if x < a then a else x
     a = 0 -- a=0 in our implementation of Taylor series
+
+calculateTermsLog :: (Real a) => a -> a -> Int
+calculateTermsLog = undefined
